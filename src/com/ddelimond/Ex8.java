666472796dafc2469b95257da3533c94deb560af
Darren Delimond <darrendelimond@gmail.com>
2023-06-03 22:02:22 -0400

    =finished

---
+++ src/com/ddelimond/Ex8.java
@@ -0,0 +1,24 @@
+package com.ddelimond;
+
+
+/*
+
+Make sure you have a class called Main
+Change package name to match yours
+Copy and paste this class to you IDE, spot and fix all errors and make sure the code runs and compiles.
+The output should be Fixed
+
+com.amigoscode;
+
+public class Main {
+    static void main(String args) {
+        System.out.println("Fixed");
+
+}
+ */
+
+public class Ex8 {
+    public static void main(String[] args) {
+        System.out.println("Fixed");
+    }
+}
