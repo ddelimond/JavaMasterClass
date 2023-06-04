c0879bf63063b0bd965bc766135df34d8f08123b
Darren Delimond <darrendelimond@gmail.com>
2023-06-03 21:52:28 -0400

    =finished

---
+++ src/com/ddelimond/Ex7.java
@@ -0,0 +1,26 @@
+package com.ddelimond;
+
+
+/*
+package com.amigoscode;
+
+public class Exercise {
+    public static void main(String[] args) {
+
+            Write a program that compares two numbers. i.e x > y
+            Create 3 variables.
+            2 to store both numbers and 1 to store the result
+            Print the result
+    }
+            }
+ */
+
+public class Ex7 {
+
+    public static void main (String[] args){
+        int x = 21;
+        int y = 30;
+        boolean  result = x < y;
+        System.out.println(result);
+    }
+}
