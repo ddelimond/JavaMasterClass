658624cc5a5aa4860c0b10f74d990bf8dd57dc21
Darren Delimond <darrendelimond@gmail.com>
2023-06-03 21:19:02 -0400

    =finished

--- src/com/ddelimond/Ex1.java
+++ src/com/ddelimond/Ex1.java
@@ -1,6 +1,6 @@
 package com.ddelimond;
 
-public class Compile {
+public class Ex1 {
     public static void main (String[] args){
         System.out.println("I compiled and ran from the terminal. Hooray");
     }
--- src/com/ddelimond/Ex2.java
+++ src/com/ddelimond/Ex2.java
@@ -17,7 +17,7 @@ public class Exercise {
 
  */
 
-public class Exercise2 {
+public class Ex2 {
     public static void main (String[] args){
        System.out.println("I love cats");
 //        System.out.println("I hate cats");
