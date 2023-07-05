2e549049f36ad06a9a4f89f4207ea69f0694a82d
Darren Delimond <darrendelimond@gmail.com>
2023-07-04 21:15:02 -0400

    =completed exercise one

---
+++ src/Classes_Objects2/Exercise1.java
@@ -0,0 +1,48 @@
+package Classes_Objects2;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Exercise1 {
+
+    public static String data = """
+            id,first_name,last_name,email,gender
+            1,Fanchette,Williamson,fwilliamson0@github.com,F
+            2,Aleksandr,Matts,amatts1@webs.com,M
+            3,Maurie,Cordero,mcordero2@google.co.jp,M
+            4,Donnajean,Crowson,dcrowson3@google.com.hk,F
+            5,Ricardo,Gofton,rgofton4@nytimes.com,M
+            6,Gabie,Tregenna,gtregenna5@guardian.co.uk,F
+            7,Marjorie,Blumsom,mblumsom6@joomla.org,F
+            8,Lester,Huyghe,lhuyghe7@jugem.jp,M
+            9,Merrily,Stangoe,mstangoe8@tiny.cc,F
+            10,Reider,Karel,rkarel9@github.io,M
+            11,Dory,Jolliff,djolliffa@wufoo.com,F
+            12,Homerus,Averay,haverayb@skyrock.com,M
+            13,Alyda,Muglestone,amuglestonec@is.gd,F
+            14,Pinchas,Louca,ploucad@google.es,M
+            15,Cherin,Eltringham,celtringhame@parallels.com,F
+            16,Mufi,Rothert,mrothertf@dropbox.com,F
+            17,Jordana,Everex,jeverexg@ucla.edu,F
+            18,Belle,Rother,brotherh@auda.org.au,F
+            19,Clevie,Sifflett,csiffletti@furl.net,M
+            20,Gretchen,Abell,gabellj@1688.com,F
+            """;
+
+
+    public  static void main(String args []) throws IOException {
+
+        File file = new File("src/Classes_Objects2/data.csv");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter writer = new FileWriter(file);
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(data);
+        printWriter.flush();
+        printWriter.close();
+
+    }
+
+}
---
+++ src/Classes_Objects2/Main.java
@@ -0,0 +1,7 @@
+package Classes_Objects2;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
